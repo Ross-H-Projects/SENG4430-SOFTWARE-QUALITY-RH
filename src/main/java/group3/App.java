16e9af3e5140728472095ee5fa384dd9412e9b54
Jack <jack.parkes@uon.edu.au>
2020-03-11 16:40:43 +1100

    Include spoon

--- src/main/java/group3/App.java
+++ src/main/java/group3/App.java
@@ -1,13 +1,14 @@
 package group3;
 
-/**
- * Hello world!
- *
- */
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	// CtClass l = Launcher.parseClass("class A { void m() { System.out.println(\"yeah\");} }");
+        // System.out.println(l);
     }
 }
