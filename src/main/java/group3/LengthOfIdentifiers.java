27b58113a6452b0afdfc2765554e2543a81aa18d
Daniel Sands <sandsd@student.chalmers.se>
2020-04-30 12:48:31 +1000

    Added some TODOs

--- src/main/java/group3/LengthOfIdentifiers.java
+++ src/main/java/group3/LengthOfIdentifiers.java
@@ -1,9 +1,19 @@
 package group3;
 
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.visitor.Query;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.util.List;
+
 public class LengthOfIdentifiers extends MetricAnalysis {
 
     @Override
     public MetricReturn performAnalysis(String fileName) {
+        Launcher launcher = Utilities.importCodeSample(fileName);
+        List<CtClass<?>> classes = Query.getElements(launcher.getFactory(), new TypeFilter<CtClass<?>>(CtClass.class));
+        //TODO: Calculate length of identifiers
         return null;
     }
 }
