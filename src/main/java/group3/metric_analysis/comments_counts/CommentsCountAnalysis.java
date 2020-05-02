51aa14ec4c939699b323a14b2b6894036f03e1d9
JacobNolan1 <jacobnolan1998@gmail.com>
2020-05-02 13:49:08 +1000

    Comment Count Analysis gets comment count per method

--- src/main/java/group3/metric_analysis/comments_counts/CommentsCountAnalysis.java
+++ src/main/java/group3/metric_analysis/comments_counts/CommentsCountAnalysis.java
@@ -1,5 +1,6 @@
 package group3.metric_analysis.comments_counts;
 
+import group3.FanOutReturn;
 import group3.MetricAnalysis;
 import group3.MetricReturn;
 import group3.Utilities;
@@ -8,18 +9,49 @@ import group3.metric_analysis.conditonal_nesting.metric_trackers.MethodTracker;
 import group3.metric_analysis.conditonal_nesting.metric_trackers.ProgramTracker;
 import org.apache.commons.lang3.ObjectUtils;
 import spoon.Launcher;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.visitor.CtIterator;
+import spoon.reflect.visitor.Query;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
 public class CommentsCountAnalysis extends MetricAnalysis {
-    private ProgramTracker programTracker;
+    private HashMap<String, HashMap<String, Integer>> classCommentsCountScores;
 
     public CommentsCountAnalysis() {
+        classCommentsCountScores = new HashMap<String, HashMap<String, Integer>>();
     }
 
     public MetricReturn performAnalysis (String fileName) {
-        return null;
+        Launcher launcher = Utilities.importCodeSample(fileName);
+
+        for (CtClass<?> classObject : Query.getElements(launcher.getFactory(), new TypeFilter<CtClass<?>>(CtClass.class))) {
+            HashMap<String, Integer> methodCommentsCountScores = new HashMap<String, Integer>();
+            for (CtMethod<?> methodObject : getMethods(classObject)) {
+                methodCommentsCountScores.put(methodObject.getSimpleName(), calculateCommentCountForMethod(methodObject));
+            }
+            classCommentsCountScores.put(classObject.getQualifiedName(), methodCommentsCountScores);
+        }
+
+        System.out.println(classCommentsCountScores);
+
+        return (MetricReturn) new FanOutReturn(classCommentsCountScores);
+    }
+
+    private int calculateCommentCountForMethod (CtMethod<?> method) {
+          return method.getComments().size();
+    }
+
+    private static ArrayList<CtMethod<?>> getMethods(CtClass<?> classObject){
+        Collection<CtMethod<?>> methodsCollection = classObject.getMethods();
+        return new ArrayList<CtMethod<?>>(methodsCollection);
     }
 }
