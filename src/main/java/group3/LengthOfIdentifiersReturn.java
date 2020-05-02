8d0db2daaf6fb7aa46822a9379e1263cb8cb450c
Daniel Sands <sandsd@student.chalmers.se>
2020-05-02 12:07:32 +1000

    changed LoIReturn class

--- src/main/java/group3/LengthOfIdentifiersReturn.java
+++ src/main/java/group3/LengthOfIdentifiersReturn.java
@@ -1,8 +1,17 @@
 package group3;
 
 public class LengthOfIdentifiersReturn extends MetricReturn {
+    private double averageLengthOfIdentifiers;
 
-    public LengthOfIdentifiersReturn(){ //TODO: Decide format of return
+    public LengthOfIdentifiersReturn(){
+        this.averageLengthOfIdentifiers = 0;
+    }
 
+    public double getAverageLengthOfIdentifiers() {
+        return averageLengthOfIdentifiers;
+    }
+
+    public void setAverageLengthOfIdentifiers(double averageLengthOfIdentifiers) {
+        this.averageLengthOfIdentifiers = averageLengthOfIdentifiers;
     }
 }
