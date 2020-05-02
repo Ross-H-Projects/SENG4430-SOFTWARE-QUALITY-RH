168d1c8ac90adc97da1eb96c6f24e23e776fe99d
Daniel Sands <sandsd@student.chalmers.se>
2020-05-02 11:12:28 +1000

    added hashmaps to store results in

--- src/main/java/group3/LengthOfIdentifiers.java
+++ src/main/java/group3/LengthOfIdentifiers.java
@@ -5,9 +5,13 @@ import spoon.reflect.declaration.CtClass;
 import spoon.reflect.visitor.Query;
 import spoon.reflect.visitor.filter.TypeFilter;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class LengthOfIdentifiers extends MetricAnalysis {
+
+    private HashMap<CtClass<?>, Double > classAverageLengthOfIdentifiers; //Here each class' average length of identifiers will be stored
+    private HashMap<CtClass<?>, String > tooShortIdentifier; //Average length of identifier doesn't say much, this will store all identifiers that have a length less than 4
 
     @Override
     public MetricReturn performAnalysis(String fileName) {
