68e4705c1a6a9b42b143e0d22709daf8be012d7c
BuildTools <unconfigured@null.spigotmc.org>
2020-05-04 14:33:23 +1000

    Couple of fanout tests

--- src/test/java/group3/FanOutTest.java
+++ src/test/java/group3/FanOutTest.java
@@ -2,6 +2,7 @@ package group3;
 
 
 import group3.metric_analysis.fan_out.FanOutAnalysis;
+import group3.metric_analysis.fan_out.FanOutTracker;
 import org.junit.Test;
 import spoon.Launcher;
 
@@ -13,14 +14,27 @@ import static org.junit.Assert.assertEquals;
 public class FanOutTest
 {
     /**
-     * Test that we can the correct result for MultipleClasses.java
+     * Test that we can the correct result for FanOut.java
      */
     @Test
     public void test_simple_case()
     {
         Launcher launcher = Utilities.importCodeSample("code_samples\\test_code_samples\\fan_out\\FanOut.java", true);
-        FanOutAnalysis tester = new FanOutAnalysis();
-        tester.performAnalysis(launcher);
-        System.out.println(tester);
+        FanOutTracker tester = new FanOutTracker(new String[] {});
+        tester.run(launcher);
+        String res = tester.toJson();
+        assertEquals("{FanOut1={test()=1, FanOut1()=0}}", res);
+    }
+
+    /**
+     * Test that commented invocations do not count as a real invocation
+     */
+    @Test
+    public void test_with_commented_invocations() {
+        Launcher launcher = Utilities.importCodeSample("code_samples\\test_code_samples\\fan_out\\FanOutCommented.java", true);
+        FanOutTracker tester = new FanOutTracker(new String[] {});
+        tester.run(launcher);
+        String res = tester.toJson();
+        assertEquals("{FanOutCommented={FanOutCommented()=0, test()=1}}", res);
     }
 }
