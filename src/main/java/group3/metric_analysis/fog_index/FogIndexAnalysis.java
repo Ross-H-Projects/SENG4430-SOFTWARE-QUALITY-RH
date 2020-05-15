df7fb7c5239c87002f4dfb61e25d612036857aa5
Daniel Sands <sandsd@student.chalmers.se>
2020-05-15 15:23:43 +1000

    added skeleton for FogIndexAnalysis and FogIndexTracker classes

---
+++ src/main/java/group3/metric_analysis/fog_index/FogIndexAnalysis.java
@@ -0,0 +1,12 @@
+package group3.metric_analysis.fog_index;
+
+import group3.MetricAnalysis;
+import spoon.Launcher;
+
+public class FogIndexAnalysis extends MetricAnalysis {
+
+    @Override
+    public void performAnalysis(Launcher launcher) {
+
+    }
+}
---
+++ src/main/java/group3/metric_analysis/fog_index/FogIndexTracker.java
@@ -0,0 +1,16 @@
+package group3.metric_analysis.fog_index;
+
+import group3.MetricTracker;
+import spoon.Launcher;
+
+public class FogIndexTracker extends MetricTracker {
+    @Override
+    public void run(Launcher launcher) {
+
+    }
+
+    @Override
+    public String toJson() {
+        return null;
+    }
+}
