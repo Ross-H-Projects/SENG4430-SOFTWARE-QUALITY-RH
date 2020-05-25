7c174c3e9be086165ec44f715fc84e7903849f88
JacobNolan1 <jacobnolan1998@gmail.com>
2020-05-25 11:13:58 +1000

    More fixies. Preeeeeyyyyy surrreeee that this should be almost it maybe

--- src/main/java/group3/metric_analysis/conditional_nesting/ConditionalNestingAnalysis.java
+++ src/main/java/group3/metric_analysis/conditional_nesting/ConditionalNestingAnalysis.java
@@ -67,9 +67,11 @@ public class ConditionalNestingAnalysis extends MetricAnalysis {
                 if(isElse) {
                     //if next statement is else.
                     currentDepth = doDepthOnCodeBlock(depth, ifStatement.getElseStatement());
+                    depthList.add(currentDepth);
                     ifStatement = null;
                 } else {
-                    currentDepth = doDepthOnCodeBlock(depth, ifStatement.getElseStatement());
+//                    currentDepth = doDepthOnCodeBlock(depth, ifStatement.getElseStatement());
+//                    depthList.add(currentDepth);
                     ifStatement = getFirstIfStatementFromCodeBlock(ifStatement.getElseStatement());
                 }
             } else {
@@ -89,96 +91,6 @@ public class ConditionalNestingAnalysis extends MetricAnalysis {
         } else {
             return null;
         }
-    }
-
-    public static boolean determineIfElementIsIf(CtElement element) {
-        for (CtIf ifStatement : Query.getElements(element, new TypeFilter<CtIf>(CtIf.class))) {
-            if (ifStatement==element) {
-                return true;
-            }
-        }
-        return false;
-    }
-    public static CtElement getTopOfIfStatementFromElseBlock(CtElement element) {
-        System.out.println("THIS IS ELEMENT" + element);
-        CtIf ifStatement = (CtIf) element.getParent();
-        CtBlock currentBlock = (CtBlock) ifStatement.getParent();
-        CtIf checkedIfStatement = ifStatement;
-        boolean atTop = false;
-        while(!atTop) {
-            if (currentBlock.getRoleInParent() == CtRole.BODY) {
-                atTop = true;
-            } else if (currentBlock.getRoleInParent() == CtRole.THEN) {
-                atTop = true;
-            } else if (currentBlock.getRoleInParent() == CtRole.ELSE) {
-                while (checkedIfStatement.getElseStatement() != null) {
-                    if (checkedIfStatement.getElseStatement() == element) {
-                        break;
-                    }
-                    CtElement checkedBlock = checkedIfStatement.getElseStatement();
-                    checkedIfStatement = Query.getElements(checkedBlock, new TypeFilter<CtIf>(CtIf.class)).get(0);
-                }
-
-                if (checkedIfStatement.getElseStatement() == null) {
-                    atTop = true;
-                    ifStatement = (CtIf)ifStatement.getParent().getParent();
-                    currentBlock = (CtBlock) ifStatement.getParent();
-                } else if (checkedIfStatement.getElseStatement() == element) {
-                    ifStatement = checkedIfStatement;
-                    System.out.println("FIRST WHILE\n" + checkedIfStatement);
-                    checkedIfStatement = (CtIf) checkedIfStatement.getParent().getParent();
-                    currentBlock = (CtBlock) checkedIfStatement.getParent();
-                    System.out.println("SECOND WHILE\n" + currentBlock);
-                }
-            }
-        }
-        System.out.println(currentBlock.getRoleInParent() + " " +ifStatement.getParent());
-        return ifStatement.getParent();
-    }
-    public static int getIfStatementDepthForMethod(CtExecutable<?> methodObject) {
-        int maxDepth = 0;
-        ArrayList<Integer> depthList = new ArrayList<Integer>();
-//        CtElement element =  Query.getElements(methodObject, new TypeFilter<CtIf>(CtIf.class)).get(5);
-//        int depthCount = getDepthOfIf(element);
-//        depthList.add(depthCount);
-        for (CtElement element : Query.getElements(methodObject, new TypeFilter<CtIf>(CtIf.class))) {
-            int depthCount = getDepthOfIf(element);
-            depthList.add(depthCount);
-        }
-        if (depthList.size() == 0) {
-            return 0;
-        } else {
-            return Collections.max(depthList);
-        }
-    }
-    public static int getDepthOfIf(CtElement element) {
-        int depthCount = 0;
-        boolean atBody = false;
-        while (!atBody) {
-//            System.out.println(element + " " + depthCount);
-            if (element.getRoleInParent() == CtRole.BODY) {
-//                System.out.println("IS BODY ");
-                atBody = true;
-            }
-            else if (element.getRoleInParent() == CtRole.STATEMENT) {
-//                System.out.println("IS STATEMENT ");
-                element = element.getParent();
-                depthCount++;
-            }
-            else if (element.getRoleInParent() == CtRole.ELSE) {
-                element = getTopOfIfStatementFromElseBlock(element);
-                if(element.getRoleInParent() == CtRole.BODY) {
-                    break;
-                }
-                depthCount++;
-            } else {
-//                System.out.println("IS THEN");
-                element = element.getParent().getParent();
-                depthCount++;
-            }
-//            System.out.println(depthCount);
-        }
-        return depthCount;
     }
     private static ArrayList<CtExecutable<?>> getMethods(CtClass<?> classObject) {
         Collection<CtMethod<?>> methodsCollection = classObject.getMethods();
