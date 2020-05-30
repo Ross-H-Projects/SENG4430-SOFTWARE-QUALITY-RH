eaccc287bb73bf5b4d439c8ed3098043b8867a7c
BuildTools <unconfigured@null.spigotmc.org>
2020-05-30 14:44:18 +1000

    Comment fan in/out, remove prints and correct JSON output

--- src/main/java/group3/metric_analysis/fan_in/FanInTracker.java
+++ src/main/java/group3/metric_analysis/fan_in/FanInTracker.java
@@ -1,5 +1,6 @@
 package group3.metric_analysis.fan_in;
 
+import com.google.gson.Gson;
 import group3.MetricTracker;
 import org.apache.commons.cli.*;
 import spoon.Launcher;
@@ -7,18 +8,27 @@ import spoon.Launcher;
 import java.util.HashMap;
 
 public class FanInTracker extends MetricTracker {
-
+    // Fan in tracker launches and reads the results of fan in analysis
     private FanInAnalysis fanInAnalysis;
 
+    // An upper threshold is used to only show methods/modules that have a fan in score lower than or equal to it
+    // A default value of 1 is reasonable, but this can easily be overwritten by the user
     private Integer upperThreshold = 1;
+
+    // 2 different modes are provided. Method mode and module mode. This is determined by a boolean value but could be
+    // adapted for many options via an enum. The method mode shows a score for each method which includes every invocation pointing to the given method.
+    // Module mode produces a score per class, where only invocations that call the given class from an external class (methods in outside classes) are counted
+    // Module mode is defaulted to false, meaning method mode is run, can be changed by user
     private Boolean moduleMode = false;
 
     public FanInTracker(String[] args)
     {
+        // Fan in specific arguments are passed in to change default parameters. Defined below and easily extendable
         Options options = new Options();
         options.addOption("max", true, "Upper threshold of fan in value to display");
         options.addOption("module", false, "Calculate fan in for module rather than methods");
 
+        // Parse cmd options
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = null;
         try {
@@ -28,6 +38,7 @@ public class FanInTracker extends MetricTracker {
             System.exit(1);
         }
 
+        // Overwrite upper threshold if option provided
         String upperThresholdArg = cmd.getOptionValue("max");
         if (upperThresholdArg != null) {
             try {
@@ -37,38 +48,41 @@ public class FanInTracker extends MetricTracker {
                 System.exit(1);
             }
         }
+        // Set module mode if option provided
         if (cmd.hasOption("module")) {
             moduleMode = true;
-            System.out.println("MODULE");
         }
+
+        // Initialise the analyser
         fanInAnalysis = new FanInAnalysis();
     }
 
     @Override
     public void run(Launcher launcher) {
+        // Run the analysis
         fanInAnalysis.performAnalysis(launcher);
     }
 
     @Override
     public String toJson() {
+        // Produce JSON output based on method/module mode choice
         if (moduleMode) {
-            return moduleModeToJSON();
+            return "{\"metric\": \"fan_in\", \"mode\": \"module\", \"result\":" + moduleModeToJSON();
         } else {
-            return methodModeToJSON();
+            return "{\"metric\": \"fan_in\", \"mode\": \"method\", \"result\":" + methodModeToJSON();
         }
     }
 
     private String methodModeToJSON() {
+        // Recreate a hashmap for each method in analysis that has a score less than or equal to the upper threshold
         HashMap<String, HashMap<String, Integer>> scores = new HashMap<>();
         HashMap<String, HashMap<String, Integer>> rawScores = fanInAnalysis.getMethodModeFanInScores();
-        System.out.println(rawScores);
         for (HashMap.Entry<String, HashMap<String, Integer>> classMap : rawScores.entrySet()) {
             String className = classMap.getKey();
             HashMap<String, Integer> methodScores = new HashMap<>();
             for (HashMap.Entry<String, Integer> methodMap : classMap.getValue().entrySet()) {
                 String methodName = methodMap.getKey();
                 Integer methodScore = methodMap.getValue();
-                System.out.println(methodScore);
                 if (methodScore <= upperThreshold) {
                     methodScores.put(methodName, methodScore);
                 }
@@ -77,21 +91,24 @@ public class FanInTracker extends MetricTracker {
                 scores.put(className, methodScores);
             }
         }
-        return scores.toString();
+        // Create JSON string of hashmap
+        Gson gson = new Gson();
+        return gson.toJson(scores);
     }
 
     private String moduleModeToJSON() {
+        // Recreate a hashmap for each class in analysis that has a score less than or equal to the upper threshold
         HashMap<String, Integer> scores = new HashMap<>();
         HashMap<String, Integer> rawScores = fanInAnalysis.getModuleModeFanInScores();
-        System.out.println(rawScores);
         for (HashMap.Entry<String, Integer> classMap : rawScores.entrySet()) {
             String className = classMap.getKey();
             Integer moduleScore = classMap.getValue();
-            System.out.println(moduleScore);
             if (moduleScore <= upperThreshold) {
                 scores.put(className, moduleScore);
             }
         }
-        return scores.toString();
+        // Create JSON string of hashmap
+        Gson gson = new Gson();
+        return gson.toJson(scores);
     }
 }
--- src/main/java/group3/metric_analysis/fan_out/FanOutAnalysis.java
+++ src/main/java/group3/metric_analysis/fan_out/FanOutAnalysis.java
@@ -13,17 +13,19 @@ import java.util.*;
 
 
 public class FanOutAnalysis extends MetricAnalysis {
+
+    // Create hashmaps for fan out analysis. One stores results on a module level (score per class on how many invocations access externally defined methods).
+    // The other stores a result per method and includes all invocations within each method
     private HashMap<String, HashMap<String, Integer>> methodModeScores;
     private HashMap<String, Integer> moduleModeScores;
 
-//    private Boolean moduleMode;
 
     public FanOutAnalysis() {
         methodModeScores = new HashMap<String, HashMap<String, Integer>>();
         moduleModeScores = new HashMap<String, Integer>();
-//        this.moduleMode = moduleMode;
     }
 
+    // Getter methods
     public HashMap<String, HashMap<String, Integer>> getMethodModeScores() {
         return methodModeScores;
     }
@@ -32,10 +34,18 @@ public class FanOutAnalysis extends MetricAnalysis {
         return moduleModeScores;
     }
 
+
+    // Main analysis method
     public void performAnalysis (Launcher launcher) {
+        // Iterate over all classes in launcher
         for (CtClass<?> classObject : Query.getElements(launcher.getFactory(), new TypeFilter<CtClass<?>>(CtClass.class))) {
+            // Create intermediate hashmap for each method within the class
             HashMap<String, Integer> methodFanOutScores = new HashMap<String, Integer>();
+            // Initialise the module score to zero
             Integer moduleModeCount = 0;
+
+            // For each constructor and method definition in class, calculate the module and method mode scores
+            // Add the method mode result to the hashmap and add the module mode score to the counter
             for (CtMethod<?> methodObject : getMethods(classObject)) {
                 methodFanOutScores.put(methodObject.getSignature(), calculateFanOutForMethod(methodObject));
                 moduleModeCount += calculateModuleFanOutForMethod(classObject, methodObject);
@@ -44,27 +54,37 @@ public class FanOutAnalysis extends MetricAnalysis {
                 methodFanOutScores.put(constructorObject.getSignature(), calculateFanOutForConstructor(constructorObject));
                 moduleModeCount += calculateModuleFanOutForConstructor(classObject, constructorObject);
             }
+
+            // Add the method mode results hashmap to the method mode scores map under the key of the class name
             methodModeScores.put(classObject.getQualifiedName(), methodFanOutScores);
+            // Add the module mode score under the key of the class name
             moduleModeScores.put(classObject.getQualifiedName(), moduleModeCount);
 
         }
     }
 
     private int calculateFanOutForMethod (CtMethod<?> method) {
+        // Count all invocations
         List<CtInvocation<?>> methodCalls = method.getElements(new TypeFilter<CtInvocation<?>>(CtInvocation.class));
         return methodCalls.size();
     }
 
     private int calculateFanOutForConstructor (CtConstructor<?> constructor) {
+        // Count all invocations
         List<CtInvocation<?>> methodCalls = constructor.getElements(new TypeFilter<CtInvocation<?>>(CtInvocation.class));
         return methodCalls.size() - 1;
     }
 
     private int calculateModuleFanOutForMethod (CtClass<?> ctClass, CtMethod<?> method) {
+        // Count only invocations that do not call local methods to class name
         Integer count = 0;
         for (CtInvocation<?> invocation : method.getElements(new TypeFilter<CtInvocation<?>>(CtInvocation.class))) {
-            String className = invocation.getExecutable().getDeclaringType().toString();
-            if (!className.equals(ctClass.getQualifiedName())) {
+            if (invocation.getExecutable().getDeclaringType() != null) {
+                String className = invocation.getExecutable().getDeclaringType().toString();
+                if (!className.equals(ctClass.getQualifiedName())) {
+                    count += 1;
+                }
+            } else {
                 count += 1;
             }
         }
@@ -72,6 +92,7 @@ public class FanOutAnalysis extends MetricAnalysis {
     }
 
     private int calculateModuleFanOutForConstructor (CtClass<?> ctClass, CtConstructor<?> constructor) {
+        // Count only invocations that do not call local methods to class name
         Integer count = 0;
         for (CtInvocation<?> invocation : constructor.getElements(new TypeFilter<CtInvocation<?>>(CtInvocation.class))) {
             String className = invocation.getExecutable().getDeclaringType().toString();
@@ -79,16 +100,17 @@ public class FanOutAnalysis extends MetricAnalysis {
                 count += 1;
             }
         }
-        System.out.println(count);
         return count - 1;
     }
 
     private static ArrayList<CtMethod<?>> getMethods(CtClass<?> classObject) {
+        // Helper method to return a list of methods within a class
         Collection<CtMethod<?>> methodsCollection = classObject.getMethods();
         return new ArrayList<CtMethod<?>>(methodsCollection);
     }
 
     private static ArrayList<CtConstructor<?>> getConstructors(CtClass<?> classObject) {
+        // Helper method to return a list of constructors within a class
         Set<? extends CtConstructor<?>> constructorCollection = classObject.getConstructors();
         return new ArrayList<CtConstructor<?>>(constructorCollection);
     }
