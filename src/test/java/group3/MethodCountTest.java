3c93061041658861ae02a6e6ef361f97f42c8c42
ZWelsh <50762478+zwelsh@users.noreply.github.com>
2020-06-05 22:16:23 +1000

    - Cyclomatic analysis refined - Cyclomatic complexity and method count added to test suite - Tests generated for both metrics - Comments added

---
+++ src/test/java/group3/MethodCountTest.java
@@ -0,0 +1,32 @@
+package group3;
+
+
+import group3.metric_analysis.cyclomatic_complexity.CyclomaticTracker;
+import org.junit.Test;
+import spoon.Launcher;
+import static org.junit.Assert.assertEquals;
+
+public class MethodCountTest
+{
+    // Test includes each of the basic components that cyclomatic complexity looks for.
+    @Test
+    public void test_zeromethods()
+    {
+        Launcher launcher = Utilities.importCodeSample("code_samples\\test_code_samples\\method_count\\method_CountTest.java", true);
+        MethodCountTracker tester = new MethodCountTracker(new String[] {});
+        tester.run(launcher);
+        int res = tester.getCount("methodCountTest");
+        assertEquals(0, res);
+    }
+
+    //Test includes no decision points, should reach base value of 1
+    @Test
+    public void test_nonzeromethods()
+    {
+        Launcher launcher = Utilities.importCodeSample("code_samples\\test_code_samples\\method_count\\method_CountTest1.java", true);
+        CyclomaticTracker tester = new CyclomaticTracker(new String[] {});
+        tester.run(launcher);
+        int res = tester.getCount("methodCountTest1");
+        assertEquals(3, res);
+    }
+}
